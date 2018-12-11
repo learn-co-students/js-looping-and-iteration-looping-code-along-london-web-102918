4524b2033b0989aefe5ed689ff42709c8cfd2117
artismarti <artismarti@gmail.com>
2018-12-11 22:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+  console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`)
+}
+return employees
+}
+function tailsNeverFails() {
+  let streak = 0;
+  while (Math.random() >= 0.5) {
+    streak ++;
+  }
+  return `You got ${streak} tails in a row!`;
+}
